0e66545e097655252ecbde236fc142e775fc8968
David <gusejnov.dn@edu.spbstu.ru>
2024-03-14 17:21:38 +0300

    commit

--- tests/models.py
+++ tests/models.py
@@ -8,9 +8,3 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
     is_correct = models.BooleanField(default=False)
-
-class TestResult(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    chosen_answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
-    is_correct = models.BooleanField()
